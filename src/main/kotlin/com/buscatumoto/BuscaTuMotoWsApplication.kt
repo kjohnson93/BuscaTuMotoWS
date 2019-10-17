3ca309e8c96e4449a152d3ee0f41aa34d80cffff
OxigenSalud <oxigensalud.android@gmail.com>
2019-10-17 14:29:09 +0200

    2q31

---
+++ src/main/kotlin/com/buscatumoto/BuscaTuMotoWsApplication.kt
@@ -0,0 +1,11 @@
+package com.buscatumoto
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BuscaTuMotoWsApplication
+
+fun main(args: Array<String>) {
+	runApplication<BuscaTuMotoWsApplication>(*args)
+}
---
+++ src/test/kotlin/com/buscatumoto/BuscaTuMotoWsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.buscatumoto
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BuscaTuMotoWsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
