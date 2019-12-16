4e1aec37a74c4b770e17d089ca18af81be28a985
kjohnson <k.johnson.kvk@gmail.com>
2019-12-16 08:05:01 +0100

    Added fix on dataclass to get first http endpoint: getByBrand

--- src/main/kotlin/com/buscatumoto/models/Schemas.kt
+++ src/main/kotlin/com/buscatumoto/models/Schemas.kt
@@ -4,11 +4,11 @@ import org.springframework.data.mongodb.core.mapping.Document
 
 data class Brand(var id: String?,var name: String?)
 
-@Document(collection = "motos")
-class Moto(var id: String?, var brand: String?, var model: String?, var imgThumbUrl: String?,
-					var modelHighlights: String?, var imgBannerUrl: String?,
-					var modelDetailHighlights: List<String?>, var priceTitle: String?, var priceDesc: String?,
-					var mainDesc: String?, var licenses: List<String?>, var licensesTitle: String?,
-					var specsTable: List<List<String?>>, var relatedItems: List<String?>,
-					var relatedItemsUrl: List<String?>) {
+@Document(collection = "motosv2")
+data class Moto(var id: String = "", var brand: String = "", var model: String = "", var imgThumbUrl: String = "",
+					var modelHighlights: String = "", var imgBannerUrl: String = "",
+					var modelDetailHighlights: List<String> = emptyList(), var priceTitle: String = "", var priceDesc: String = "",
+					var mainDesc: String = "", var licenses: List<String> = emptyList(), var licensesTitle: String = "",
+					var specsTable: List<List<String>> = emptyList(), var relatedItems: List<String> = emptyList(),
+					var relatedItemsUrl: List<String> = emptyList()) {
 }
